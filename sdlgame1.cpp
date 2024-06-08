8f9d94d9f2ea6fef39355cc31b375d1560beda17
Steven Degutis <sbdegutis@gmail.com>
2024-06-08 06:29:38 -0500

    Need a game loop...

--- sdlgame1.cpp
+++ sdlgame1.cpp
@@ -8,18 +8,21 @@ void sdlgame1::run() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) return;
 	SDL_Window* win = SDL_CreateWindow("Game1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 400, SDL_WINDOW_SHOWN);
 	if (win == NULL) return;
+
 	SDL_Surface* surf = SDL_GetWindowSurface(win);
-	SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0x33, 0x99, 0xff));
-
-	SDL_Rect rect = { 10,20,300,200 };
-	SDL_FillRect(surf, &rect, SDL_MapRGBA(surf->format, 0xff, 0xff, 0xff, 0x00));
-
-	SDL_UpdateWindowSurface(win);
+	int x = 0;
 
 	auto running = true;
 	while (running) {
 		SDL_Event event;
+
+		SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0x33, 0x99, 0xff));
+		SDL_Rect rect = { x++,20,300,200 };
+		SDL_FillRect(surf, &rect, SDL_MapRGBA(surf->format, 0xff, 0xff, 0xff, 0x00));
+		SDL_UpdateWindowSurface(win);
+
 		while (SDL_PollEvent(&event)) {
+
 			switch (event.type) {
 			case SDL_QUIT:
 				running = false;
