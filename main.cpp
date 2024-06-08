a28828dea2f61b2e45faebd759a69dd1d8fb57b6
Steven Degutis <sbdegutis@gmail.com>
2024-06-07 21:04:45 -0500

    It actually works too...!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <SDL.h>
 #undef main
 
+#include <Box2D.h>
+
 extern "C"
 {
 	#include <lauxlib.h>
@@ -11,6 +13,45 @@ int main() {
 	lua_State* lua = luaL_newstate();
 	luaL_openlibs(lua);
 	lua_close(lua);
+
+	b2Vec2 gravity(0.0f, -10.0f);
+	b2World world(gravity);
+
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, -10.0f);
+
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+	b2PolygonShape groundBox;
+	groundBox.SetAsBox(50.0f, 10.0f);
+
+	groundBody->CreateFixture(&groundBox, 0.0f);
+
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(0.0, 4.0f);
+	b2Body* body = world.CreateBody(&bodyDef);
+
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(1.0f, 1.0f);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+
+	body->CreateFixture(&fixtureDef);
+
+	float timeStep = 1.0f / 60.0f;
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
+
+	for (int32 i = 0; i < 60; ++i) {
+		world.Step(timeStep, velocityIterations, positionIterations);
+		b2Vec2 position = body->GetPosition();
+		float angle = body->GetAngle();
+		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+	}
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
 	SDL_Window* win = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 900, 400, SDL_WINDOW_SHOWN);
