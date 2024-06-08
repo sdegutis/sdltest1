a8a8b92264ee18daf37f77fccb7c776da970f21e
Steven Degutis <sbdegutis@gmail.com>
2024-06-07 20:43:22 -0500

    Lua also for some reason.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <SDL.h>
 #undef main
 
+extern "C"
+{
+	#include <lauxlib.h>
+	#include <lualib.h>
+}
+
 int main() {
+	lua_State* lua = luaL_newstate();
+	luaL_openlibs(lua);
+	lua_close(lua);
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
 	SDL_Window* win = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 900, 400, SDL_WINDOW_SHOWN);
 	if (win == NULL) return 1;
 	SDL_Surface* surf = SDL_GetWindowSurface(win);
-	SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0x00, 0x00, 0xff));
+	SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0xff, 0x00, 0x00));
 	SDL_UpdateWindowSurface(win);
 	SDL_Delay(2000);
 	SDL_DestroyWindow(win);
