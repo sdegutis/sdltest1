e42128a86601b884bb84f6b1a132824559fd5f70
Steven Degutis <sbdegutis@gmail.com>
2024-06-08 07:41:10 -0500

    Basic game loop at 30fps?

--- sdlgame1.cpp
+++ sdlgame1.cpp
@@ -12,14 +12,22 @@ void sdlgame1::run() {
 	SDL_Surface* surf = SDL_GetWindowSurface(win);
 	int x = 0;
 
+	auto frameDuration = 33;
+	auto lastFrame = SDL_GetTicks64();
+
 	auto running = true;
 	while (running) {
 		SDL_Event event;
 
-		SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0x33, 0x99, 0xff));
-		SDL_Rect rect = { x++,20,300,200 };
-		SDL_FillRect(surf, &rect, SDL_MapRGBA(surf->format, 0xff, 0xff, 0xff, 0x00));
-		SDL_UpdateWindowSurface(win);
+		auto newFrame = SDL_GetTicks64();
+		if (newFrame >= lastFrame + frameDuration) {
+			lastFrame = newFrame;
+
+			SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0x33, 0x99, 0xff));
+			SDL_Rect rect = { x++,20,300,200 };
+			SDL_FillRect(surf, &rect, SDL_MapRGBA(surf->format, 0xff, 0xff, 0xff, 0x00));
+			SDL_UpdateWindowSurface(win);
+		}
 
 		while (SDL_PollEvent(&event)) {
 
