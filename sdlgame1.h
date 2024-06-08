cf9fc4819448543179dee00c1c760a6cbe2bed34
Steven Degutis <sbdegutis@gmail.com>
2024-06-08 06:16:21 -0500

    Random testing...

--- sdlgame1.h
+++ sdlgame1.h
@@ -11,18 +11,29 @@ public:
 
 
 		if (SDL_Init(SDL_INIT_VIDEO) < 0) return;
-		SDL_Window* win = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 900, 400, SDL_WINDOW_SHOWN);
+		SDL_Window* win = SDL_CreateWindow("Game1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 400, SDL_WINDOW_SHOWN);
 		if (win == NULL) return;
 		SDL_Surface* surf = SDL_GetWindowSurface(win);
-		SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0xff, 0x00, 0x00));
+		SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 0x33, 0x99, 0xff));
+
+		SDL_Rect rect = {10,20,300,200};
+		SDL_FillRect(surf, &rect, SDL_MapRGBA(surf->format, 0xff, 0xff, 0xff, 0x00));
+
 		SDL_UpdateWindowSurface(win);
 
 		auto running = true;
 		while (running) {
 			SDL_Event event;
 			while (SDL_PollEvent(&event)) {
-				if (event.type == SDL_QUIT) {
+				switch (event.type) {
+				case SDL_QUIT:
 					running = false;
+					break;
+				case SDL_KEYDOWN:
+					if (event.key.keysym.sym == SDLK_SPACE) {
+						running = false;
+					}
+					break;
 				}
 			}
 		}
